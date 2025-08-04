91b9ae7bc2073eff2e909bc5610fbcc1fbba979c
Shiva Yadav <shivaydv1256@gmail.com>
2025-08-04 21:21:27 +0530

    vercel analytics added

--- src/analytics/analytics.tsx
+++ src/analytics/analytics.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import MSClarity from './MsClarity'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import React from "react";
+import { Analytics as Vercel } from "@vercel/analytics/next";
+import MSClarity from "./MsClarity";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const Analytics = () => {
-    
   return (
     <>
-     <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
-    <MSClarity/>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
+      <MSClarity />
+      <Vercel />
     </>
-  )
-}
+  );
+};
 
-export default Analytics
+export default Analytics;
